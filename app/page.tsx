0a4f9a2c31850cc0ea3f8c531fe6bbcfca6bceb8
Michael Beasley <beasleybytes@gmail.com>
2024-01-15 10:03:42 -0500

    added markdown processing logic and tested mapping through content folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col content-center justify-center flex-wrap">
+    <main className="px-16 py-10 flex flex-col content-center justify-center flex-wrap">
       <h2>Test</h2>
     </main>    
   )
